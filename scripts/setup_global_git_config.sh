eaa848b20280acb169364e00ca6534f10401e5af
simaxme <contact@simax.me>
2025-12-15 19:55:48 +0100

    Added tag.gpgsign option to global git config

--- scripts/setup_global_git_config.sh
+++ scripts/setup_global_git_config.sh
@@ -8,5 +8,6 @@ git config --global merge.tool nvimdiff
 
 # Signing
 git config --global commit.gpgsign true
+git config --global tag.gpgsign true
 git config --global gpg.format ssh
 git config --global user.signingkey=~/.ssh/signing_key.pub
