0d5f4e38823f9d0481995911c04a3b1e4d5134fd
simaxme <contact@simax.me>
2025-12-15 18:14:51 +0100

    Added small script to setup my recommended global git config

---
+++ scripts/setup_global_git_config.sh
@@ -0,0 +1,12 @@
+git config --global core.excludesfile ~/.gitignore
+git config --global push.autosetupremote true
+git config --global pull.rebase true
+
+# Editor
+git config --global core.editor nvim
+git config --global merge.tool nvimdiff
+
+# Signing
+git config --global commit.gpgsign true
+git config --global gpg.format ssh
+git config --global user.signingkey=~/.ssh/signing_key.pub
